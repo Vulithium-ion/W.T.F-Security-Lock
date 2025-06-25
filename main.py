cb7e0c7c63b00730cb4064e8285988078524e5b0
user.name <user.email>
2025-06-26 07:11:25 +0800

    Listen 2 accessing

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import face_utils
+import time
+#import face_utils
+import inotify.adapters
 
 
 def main():
-    face_utils.check_face_camera()
+    # face_utils.check_face_camera()
+    
+    file_to_watch = '/home/Fox/code/WTF_Security_Lock/'
+
+    i = inotify.adapters.Inotify()
+    i.add_watch('{}'.format(file_to_watch))
+
+    print("Listening for events in {}".format(file_to_watch))
+    for event in i.event_gen(yield_nones=False):
+        (_, event_types, path, filename) = event
+        if 'IN_ACCESS' in event_types:
+            print(f"🔔 目录访问了: {path}")
+        elif 'IN_OPEN' in event_types:
+            print(f"📂 目录被打开: {path}")
 
 
 if __name__ == '__main__':
