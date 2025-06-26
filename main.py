375e8a12a846648d3b3672d062888f307dc1b2d1
user.name <user.email>
2025-06-27 00:41:23 +0800

    wtf?>????

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ stopEvent = threading.Event()
 isUser = False
 cap = cv2.VideoCapture(0)
 check_once = None
+lockOn = False
 
 
 class FullscreenWindow(QWidget):
@@ -23,49 +24,34 @@ class FullscreenWindow(QWidget):
         self.setWindowState(Qt.WindowFullScreen)
 
 
-# def check_face_once():
-#     print("Opened! check!")
-#     global cap
-#     cap.grab()
-#     ret, frame = cap.read()
-#     if(ret):
-#         cv2.imwrite("./test.jpg", frame)
-#         if(not face_utils.check_face("./test.jpg")):
-#             print("attacker!")
-#             #app.open_window()
-#         else:
-#             print("User!")
-#     time.sleep(100)
+class ListenThread(QThread):
+    signal_start = pyqtSignal()
 
+    def check_face_once(self):
+        global lockOn
+        global isUser
+        if(not isUser and not lockOn):
+            self.signal_start.emit()
 
-def check_face_once():
-    global isUser
-    if(not isUser):
-        app.start_lock()
-    time.sleep(100)
+    def run(self):
+        global check_once
+        file_to_watch = '/home/Fox/code/tmp/'
+        i = inotify.adapters.Inotify()
+        i.add_watch('{}'.format(file_to_watch))
 
-
-def listen():
-    global check_once
-    file_to_watch = '/home/Fox/code/tmp/'
-    i = inotify.adapters.Inotify()
-    i.add_watch('{}'.format(file_to_watch))
-
-    print("Listening for events in {}".format(file_to_watch))
-    for event in i.event_gen(yield_nones=True):
-        start_time = time.time()
-        if(event):
-            (_, event_types, path, filename) = event
-            if 'IN_ACCESS' in event_types or 'IN_OPEN' in event_types:
-                print("目录访问了: {}, {}".format(path, start_time))
-                if(check_once is None or not check_once.is_alive()):
-                    check_once = threading.Thread(target=check_face_once)
-                    check_once.start()
-        else:
-            if stopEvent.wait(
-                timeout=max(0, .01 - time.time() + start_time)
-            ):
-                break
+        print("Listening for events in {}".format(file_to_watch))
+        for event in i.event_gen(yield_nones=True):
+            start_time = time.time()
+            if(event):
+                (_, event_types, path, filename) = event
+                if 'IN_ACCESS' in event_types or 'IN_OPEN' in event_types:
+                    print("目录访问了: {}, {}".format(path, start_time))
+                    self.check_face_once()
+            else:
+                if stopEvent.wait(
+                    timeout=max(0, .01 - time.time() + start_time)
+                ):
+                    break
 
 
 def check_face_loop():
@@ -89,31 +75,18 @@ def check_face_loop():
     print("check quited")
 
 
-def mask():
-    global checkInterval
-    checkInterval = 3
-    app = QApplication(sys.argv)
-    window = FullscreenWindow()
-    window.show()
-    sys.exit(app.exec_())
-    checkInterval = 5
-
-
 class WorkerThread(QThread):
-    # def run(self):
-    #     print("启动！")
-    #     self.signal_open_window.emit()
-    #     self.sleep(5)
-    #     print("关闭！")
-    #     self.signal_close_window.emit()
-    #     self.sleep(5)
-    #     print("退出！")
-    #     self.signal_exit_program.emit()
+    signal_stop = pyqtSignal()
 
     def run(self):
         print("子线程启动！！")
         while(True):
             print(isUser)
+            if(isUser):
+                print("emit!")
+                self.signal_stop.emit()
+                print("emit done!")
+                return 
             time.sleep(1)
 
 
@@ -122,18 +95,30 @@ class Main:
         self.app = QApplication(sys.argv)
         self.window = None
         self.worker_thread = None
-        self.listener = threading.Thread(target=listen)
+        self.listener = ListenThread()
         self.checker = threading.Thread(target=check_face_loop)
     
-    
     def start_lock(self):
+        global lockOn
+        lockOn = True
         self.open_window()
         global checkInterval
         checkInterval = 2
         if(self.worker_thread is None or not self.worker_thread.isRunning()):
             self.worker_thread = WorkerThread()
+            self.worker_thread.signal_stop.connect(self.quit_lock)
             self.worker_thread.start()
 
+    def quit_lock(self):
+        print("quit lock!")
+        global lockOn
+        global checkInterval
+        lockOn = False
+        checkInterval = 5
+        self.worker_thread.quit()
+        self.worker_thread.wait()
+        self.worker_thread = None
+        self.close_window()
     
     def open_window(self):
         if not self.window or not self.window.isVisible():
@@ -150,17 +135,20 @@ class Main:
         global stopEvent
         stopEvent.set()
         self.checker.join()
-        self.listener.join()
+        if self.listener.isRunning():
+            self.listener.quit()
+            self.listener.wait()
         if self.worker_thread.isRunning():
             self.worker_thread.quit()
             self.worker_thread.wait()
-        self.app.quit()
         global cap
         cap.release()
+        self.app.quit()
 
     def run(self):
         self.checker.start()
         self.listener.start()
+        self.listener.signal_start.connect(self.start_lock)
         print("开始监听！")
         sys.exit(self.app.exec_())
 
