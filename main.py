1ebf8cef12fb8e9413ecedbe9d99225e93d39604
user.name <user.email>
2025-06-26 18:28:25 +0800

    check face for every 5 seconds

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
-#import face_utils
+import threading
+import face_utils
 import inotify.adapters
 
 
-def main():
-    # face_utils.check_face_camera()
-    
-    file_to_watch = '/home/Fox/code/WTF_Security_Lock/'
+CHECK_INTERVAL = 5
+stopEvent = threading.Event()
 
+
+def listen():
+    file_to_watch = '/home/Fox/code/WTF_Security_Lock/'
     i = inotify.adapters.Inotify()
     i.add_watch('{}'.format(file_to_watch))
 
@@ -18,6 +20,35 @@ def main():
             print(f"🔔 目录访问了: {path}")
         elif 'IN_OPEN' in event_types:
             print(f"📂 目录被打开: {path}")
+
+
+def check_face():
+    while(not stopEvent.is_set()):
+        start_time = time.time()
+        
+        face_utils.check_face_camera()
+
+        if stopEvent.wait(
+            timeout=max(0, CHECK_INTERVAL - time.time() + start_time)
+        ):
+            break
+
+    print("quit")
+
+
+def main():
+    listener = threading.Thread(target=listen)
+    checker = threading.Thread(target=check_face)
+
+    checker.start()
+    try:
+        while True:
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("主线程捕获到 KeyboardInterrupt，发送停止信号...")
+        stopEvent.set()
+
+    checker.join()
 
 
 if __name__ == '__main__':
